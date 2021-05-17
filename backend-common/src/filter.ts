e6b05ce46ef2eebbf76ad70e758ee7caa862321b
Yasser Fadl <yasser.fadl@gmail.com>
2021-05-17 23:04:09 +0200

    switching to bulk filter

--- backend-common/src/filter.ts
+++ backend-common/src/filter.ts
@@ -1,15 +1,23 @@
-export interface Filter {
-  field: string
-  operator: 'contains' | 'eq' | 'ne'
-  value: string | null
+export type FilterSubExpressions = {
+  conditionType?: 'AND' | 'OR',
+  filter?: string
+  expressions?: FilterSubExpressions[]
+  field?: string
+  operator?: 'eq' | 'ne' | 'lt' | 'lte' | 'gt' | 'gte' | 'contains' | 'after' | 'on' | 'before' | 'includes' | 'excludes'
+  value?: string | number | Date
+}
+
+export type FilterExpression = FilterSubExpressions[]
+
+export interface SortField {
+  key: string
+  order: 'asc' | 'desc'
 }
 
 export interface BulkFilter {
+  freeText?: string
+  filters?: FilterExpression
+  sort?: SortField
   limit?: number
   offset?: number
-  sort?: {
-    key: string
-    order: 'asc' | 'desc'
-  }
-  filters: Filter[]
 }
