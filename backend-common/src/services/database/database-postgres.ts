926d3803de2f630b90b5687f599de73a34db9e7b
Yasser Fadl <yasser.fadl@gmail.com>
2021-06-05 10:45:36 +0200

    fixing up crud operations a bit

--- backend-common/src/services/database/database-postgres.ts
+++ backend-common/src/services/database/database-postgres.ts
@@ -1,11 +1,12 @@
 import pg from 'pg'
 import { QueryResult } from 'pg'
 import { Logger } from 'pino'
+import { snakeCase } from 'snake-case'
 
 // @ts-ignore
 import { inject } from 'pg-camelcase'
 import { DatabasePostgresPool } from './database-postgres-pool'
-import { createFilters, exactlyOneResult, selectFields } from './database-utils'
+import { createFilters, createInsert, exactlyOneResult, selectFields } from './database-utils'
 inject(pg)
 
 export class DatabasePostgres<Tables extends string> {
@@ -24,17 +25,45 @@ export class DatabasePostgres<Tables extends string> {
   public async crudGet <T extends object>(table: Tables, fields: readonly (keyof T)[], filters: Record<string, any>, notSingleError: Error): Promise<Pick<T, typeof fields[number]>> 
   public async crudGet <T extends object>(table: Tables, fields: readonly (keyof T)[], filters: Record<string, any>, notSingleError?: undefined | Error): Promise<Pick<T, typeof fields[number]> | Pick<T, typeof fields[number]>[]> {
     const { filter, filterValues } = createFilters({
-      filters: Object.entries(filters).map(([field, value]) => ({ value, field, operator: 'eq' }))
+    filters: Object.entries(filters).map(([field, value]) => ({ value, field, operator: 'eq' }))
     })
     const result = await this.query<Pick<T, typeof fields[number]>>(`
       SELECT ${selectFields<T>(fields, 'samf')}
-      FROM "${table}"
+      FROM "app"."${table}"
       ${filter}
     `, filterValues)
     if (notSingleError) {
       return exactlyOneResult(result.rows, notSingleError)
     }
     return result.rows
+  }
+
+  public async crudInsert <T extends object>(table: Tables, insert: Partial<Record<keyof T, string | number | string[] | Date | null | undefined>>): Promise<void>
+  public async crudInsert <T extends object>(table: Tables, insert: Partial<Record<keyof T, string | number | string[] | Date | null | undefined>>, returns: readonly (keyof T)[]): Promise<Record<keyof T, any>>
+  public async crudInsert <T extends object>(table: Tables, insert: Partial<Record<keyof T, string | number | string[] | Date | null | undefined>>, returns?: readonly (keyof T)[]): Promise<void | Record<keyof T, any>> {
+    const [keys, values, realValues] = createInsert(insert)
+    if (returns) {
+      const returnStatement = (returns || []).map(key => snakeCase(key.toString())).join(',')
+      const result = await this.query<Pick<T, typeof returns[number]>>(`INSERT INTO "app".${table}(${keys}) VALUES (${values}) RETURNING ${returnStatement};`, realValues)
+      return exactlyOneResult(result.rows, new Error())
+    } else {
+      await this.query(`INSERT INTO "app".${table}(${keys}) VALUES (${values})`, realValues)
+    }
+  }
+
+  public async crudUpdate <T extends object>(table: Tables, update: Partial<Record<keyof T, string | number | string[] | Date | null | undefined>>, filters: Record<string, any>, error: Error): Promise<void> {
+    const { filter, filterValues } = createFilters({
+      filters: Object.entries(filters).map(([field, value]) => ({ value, field, operator: 'eq' }))
+    })
+    const [keys, values, realValues] = createInsert(update, filterValues.length)
+    const result = await this.query(`
+        UPDATE "app".${table}
+        SET (${keys}) = row(${values})
+        ${filter}
+    `, [...filterValues, ...realValues])
+    if (result.rowCount !== 1) {
+      throw error
+    }
   }
 
   public async transaction<T> (callback: () => Promise<T>): Promise<T> {
