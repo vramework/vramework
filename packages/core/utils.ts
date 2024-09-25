fbf7ae58b48c3d8e686f708e98acb0aed6680807
Yasser Fadl <yasser.fadl@gmail.com>
2024-09-25 11:16:45 +0100

    refactor: use path to regex to inject variables into path

--- packages/core/utils.ts
+++ packages/core/utils.ts
@@ -1,24 +1,23 @@
 import { RequestHeaders } from './types'
+import { compile } from 'path-to-regexp'
 
 export const injectIntoUrl = (route: string, keys: Record<string, string>) => {
-  for (const [name, value] of Object.entries(keys)) {
-    route = route.replace(`:${name}`, value)
-  }
-  return route
+  const path = compile(route)
+  return path(keys)
 }
 
-export const getHeader = (
-  headers: RequestHeaders,
-  name: string
-): string | undefined => {
-  let value: string | string[] | undefined
-  if (typeof headers === 'function') {
-    value = headers(name)
-  } else {
-    value = headers[name]
-  }
-  if (value instanceof Array) {
-    throw new Error('Array header values not yet supported')
-  }
-  return value
-}
+// export const getHeader = (
+//   headers: RequestHeaders,
+//   name: string
+// ): string | undefined => {
+//   let value: string | string[] | undefined
+//   if (typeof headers === 'function') {
+//     value = headers(name)
+//   } else {
+//     value = headers[name]
+//   }
+//   if (value instanceof Array) {
+//     throw new Error('Array header values not yet supported')
+//   }
+//   return value
+// }
