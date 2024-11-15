231246bd0f739f96566bd3f3f118942df43cc313
Yasser Fadl <yasser.fadl@gmail.com>
2024-11-15 15:27:49 +0100

    revert: coerce array types if needed via ajv validation

--- packages/core/src/schema.ts
+++ packages/core/src/schema.ts
@@ -10,7 +10,7 @@ import { getRoutes } from './route-runner.js'
 
 const ajv = new Ajv({
   removeAdditional: false,
-  coerceTypes: "array",
+  coerceTypes: true,
 })
 addFormats.default(ajv as any)
 
@@ -21,9 +21,12 @@ const validators = new Map<string, ValidateFunction>()
  * @returns A map of schemas.
  */
 const getSchemas = () => {
+  // @ts-ignore
   if (!global.schemas) {
+    // @ts-ignore
     global.schemas = new Map<string, any>()
   }
+  // @ts-ignore
   return global.schemas
 }
 
